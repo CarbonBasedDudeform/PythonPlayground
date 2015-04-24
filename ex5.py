1c7e6a730eeb6bfcb7d3ba99bc8c750a9e96e588
dom <contact@dominicmusgrave.co.uk>
2015-04-24 19:01:50 +0100

    ex5 of lpthw

---
+++ ex5.py
@@ -0,0 +1,15 @@
+my_name = "Dom"
+my_age = 24
+my_height = 1.7 #metres
+my_weight = 75 #kg
+my_eyes = "brown"
+my_teeth = "brown"
+my_hair = "brown"
+
+print "Let's talk about %s" % my_name
+print "He's %d metres tall" % my_height
+print "He's %d kg heavy" % my_weight
+print "Actually, that's not too heavy"
+print "He's got %s eyes and %s hair" % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee and cigarettes" % my_teeth
+print "If i add %d, %d, and %d, I get %d" % (my_age, my_height, my_weight, my_age + my_height + my_weight)
